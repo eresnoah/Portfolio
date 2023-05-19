408e7d4bdeacf702ee37100df5e3a171f82bbb40
eresnoah <eresnoah@gmail.com>
2023-05-19 16:47:58 +0200

    sds

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,6 @@
-const express = require("express");
-const next = require("next");
-const zod = require("zod");
+import express from "express";
+import next from "next";
+import zod from "zod";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -12,25 +12,10 @@ interface Todo {
   deadline: Date;
 }
 
-const todos: Todo[] = [
-  {
-    id: 0,
-    text: "learn",
-    deadline: new Date(),
-  },
-  {
-    id: 1,
-    text: "learn",
-    deadline: new Date(),
-  },
-];
-
 app
   .prepare()
   .then(() => {
     const server = express();
-
-    server.use(express.json());
 
     server.get("*", (req: any, res: any) => {
       console.log("app get request");
@@ -41,8 +26,7 @@ app
       console.log("post req");
     });
 
-    server.listen(3000, (err: any) => {
-      if (err) throw err;
+    server.listen(3000, () => {
       console.log("> Ready on http://localhost:3000");
     });
   })
